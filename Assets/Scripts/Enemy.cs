b2d92bd8c60b2192eabf6bb54dc74c8ab2f3b7d0
iliaakm <iliaakm@gmail.com>
2020-02-24 19:58:49 +0300

    add enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Side
+{
+    None, Blue, Red
+}
+
+public class Enemy : MonoBehaviour
+{
+    public Side _side;
+    public float radius;
+    public float speed;
+}
