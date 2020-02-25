22cf6b2e98b4feae05e17ba210bd52605ddc5803
iliaakm <iliaakm@gmail.com>
2020-02-25 13:38:18 +0300

    replace values on class config in spawn

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class SceneController : MonoBehaviour
 {
+    public MainController _mainController;
     [SerializeField]
     Transform _bg;
     [SerializeField]
@@ -19,6 +20,8 @@ public class SceneController : MonoBehaviour
     public float minRadius;
     public UnityAction onReady;
 
+    Coroutine cor;
+
     public void SetBGScale(int x, int y)
     {
         _bg.localScale = new Vector3(x, y, 1);
@@ -32,7 +35,9 @@ public class SceneController : MonoBehaviour
 
     public void Spawn(GameConfig config)
     {
-        StartCoroutine(SpawnRoutine(config));
+        if (cor != null)
+            StopCoroutine(cor);
+        cor = StartCoroutine(SpawnRoutine(config));
     }
 
     IEnumerator SpawnRoutine(GameConfig config)
@@ -46,15 +51,11 @@ public class SceneController : MonoBehaviour
 
         for (int i = 0; i < count; i++)
         {
-            Enemy blueOne = InstatiateUnitRandom(radiusMinMax, speedMinMax, cellSize, Side.Blue);            //спавним синих
-            blueOne.GetComponent<SpriteRenderer>().color = Color.blue;
-            blueOne.name = "Blue_" + i;
-            _units.Add(blueOne);
+            string name = "Blue_" + i;
+            Enemy blueOne = InstatiateUnitRandom(name, radiusMinMax, speedMinMax, cellSize, Side.Blue);            //спавним синих
 
-            Enemy redOne = InstatiateUnitRandom(radiusMinMax, speedMinMax, cellSize, Side.Red);            //спавним красных
-            redOne.GetComponent<SpriteRenderer>().color = Color.red;
-            redOne.name = "Red_" + i;
-            _units.Add(redOne);
+            name = "Red_" + i;
+            Enemy redOne = InstatiateUnitRandom(name, radiusMinMax, speedMinMax, cellSize, Side.Red);            //спавним красных
 
             yield return new WaitForSeconds(delay);
         }
@@ -88,7 +89,7 @@ public class SceneController : MonoBehaviour
             pos = new Vector2(X, Y);
             for (int i = 0; i < _units.Count; i++)
             {
-                if (Vector2.Distance(_units[i].pos, pos) < (radius + _units[i]._radius) / 2)
+                if (Vector2.Distance(_units[i]._pos, pos) < (radius + _units[i]._radius) / 2)
                 {
                     check = true;
                     break;
@@ -99,35 +100,58 @@ public class SceneController : MonoBehaviour
         return pos;
     }
 
-    Enemy InstatiateUnitRandom(Vector2 radius, Vector2 speed, Vector2 cellSize, Side side)
+    Enemy InstatiateUnitRandom(string name, Vector2 radius, Vector2 speed, Vector2 cellSize, Side side)
     {
-        float _radius = Random.Range(radius.x, radius.y);
-        float _speed = Random.Range(speed.x, speed.y);
-        Vector2 pos = GetRandomPos(_radius);
+        SaveConfig unitConfig = new SaveConfig();
+        unitConfig.id = name;
+        unitConfig.radius = Random.Range(radius.x, radius.y);
+        unitConfig.speed = Random.Range(speed.x, speed.y);
+        unitConfig.pos = GetRandomPos(unitConfig.radius);
+        unitConfig.side = side;
 
-        return InstatiateUnit(pos, _radius, _speed, cellSize, side);
+        return InstatiateUnit(unitConfig, cellSize);
     }
 
-    Enemy InstatiateUnit(Vector2 pos, float radius, float speed, Vector2 cellSize, Side side)
+    Enemy InstatiateUnit(SaveConfig unitConfig, Vector2 cellSize)
     {
         GameObject unit = Instantiate(_unitPref, _gameArea, false);
-        unit.transform.localPosition = pos;
+        unit.name = unitConfig.id;
         unit.transform.rotation = Quaternion.identity;
+
         Enemy enemy = unit.GetComponent<Enemy>();
-        enemy._speed = speed;
-        enemy.SetRadius(radius);
-        enemy._side = side;
+        enemy.SetPos(unitConfig.pos);
+        enemy.SetRadius(unitConfig.radius);
+        enemy._speed = unitConfig.speed;
+        enemy._side = unitConfig.side;
         enemy._areaSize = _areaSize;
         enemy._cellSize = cellSize;
+        unit.GetComponent<SpriteRenderer>().color = (enemy._side == Side.Blue) ? Color.blue : Color.red;
+        _units.Add(enemy);
 
         return unit.GetComponent<Enemy>();
     }
 
-    public void Load(SaveConfigList saves)
+    public void Load(SaveConfigList saves, float delay)
     {
+        if (cor != null)
+            StopCoroutine(cor);
+        cor = StartCoroutine(LoadRountine(saves, delay));
+    }
+
+    IEnumerator LoadRountine(SaveConfigList saves, float delay)
+    {
+        for (int i = 0; i < _units.Count; i++)
+            DestroyImmediate(_units[i].gameObject);
+
+        _units.Clear();
+        float radiusMinMax = _mainController._gameConfig.unitSpawnMaxRadius;
+        Vector2 cellSize = new Vector2( radiusMinMax, radiusMinMax);
+
         for (int i = 0; i < saves.saveConfigs.Length; i++)
         {
-            print(saves.saveConfigs[i].id);
+            InstatiateUnit(saves.saveConfigs[i], cellSize);
+            yield return new WaitForSeconds(delay);
+
         }
 
         onReady.Invoke();
