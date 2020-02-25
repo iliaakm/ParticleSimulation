7656252ad79da0041245fac75fa723b2ca985e7e
iliaakm <iliaakm@gmail.com>
2020-02-25 11:49:20 +0300

    add save option

--- Assets/Scripts/IOController.cs
+++ Assets/Scripts/IOController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class IOController: MonoBehaviour
+public class IOController : MonoBehaviour
 {
+    public SceneController _sceneController;
     const string _gameConfigURL = "GameConfig.json";        //конфиг игры
+    const string _savePattern = "EnemySave";                 //название сохранения
 
     public GameConfig ParseGameConfig()                     //читаем конфиг
     {
@@ -17,10 +19,36 @@ public class IOController: MonoBehaviour
 
     public void Save()
     {
+        int unitN = _sceneController._units.Count;
+        SaveConfigList saves = new SaveConfigList();
+        saves.saveConfigs = new SaveConfig[unitN];
 
+        for (int i = 0; i < unitN; i++)
+        {
+            Enemy enemy = _sceneController._units[i];
+            saves.saveConfigs[i] = new SaveConfig();
+            saves.saveConfigs[i].id = enemy.name;
+            saves.saveConfigs[i].pos = enemy.pos;
+            saves.saveConfigs[i].side = enemy._side;
+            saves.saveConfigs[i].radius = enemy._radius;
+            saves.saveConfigs[i].speed = enemy._speed;
+        }
+
+        string json = JsonUtility.ToJson(saves);
+       
+        PlayerPrefs.SetString(_savePattern, json);
+        PlayerPrefs.Save();
+        print("saved");
     }
 
     public void Load()
     {
+        string json = PlayerPrefs.GetString(_savePattern);
+        SaveConfigList saves = new SaveConfigList();
+        saves = JsonUtility.FromJson<SaveConfigList>(json);
+        for (int i = 0; i < saves.saveConfigs.Length; i++)
+        {
+            print(saves.saveConfigs[i].id);
+        }
     }
 }
