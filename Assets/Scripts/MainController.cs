d4f3be697b3128bbc6efcb8d29bde0123eb4fed3
iliaakm <iliaakm@gmail.com>
2020-02-25 12:30:12 +0300

    add state maschine

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum State
+{
+    Idle,
+    Loading,
+    Ready,
+    Playing,
+    Over
+}
+
 public class MainController : MonoBehaviour
 {
-public
-    IOController ioController;
+    public IOController ioController;
 
-    public 
-    SceneController _sceneController;
+    public SceneController _sceneController;
 
-    public
-    PhysicsController _physicsController;
+    public PhysicsController _physicsController;
 
-  public
-    UIController _uiController;
+    public UIController _uiController;
+
+    State state;
+
+    public State _State
+    {
+        get { return state; }
+        set
+        {
+            state = value;
+            OnStateChange();
+        }
+    }
 
     float _timeStart;
 
@@ -23,11 +40,14 @@ public
     {
         GameConfig _gameConfig = ioController.ParseGameConfig();
         Init(_gameConfig);
-        _sceneController.Spawn(_gameConfig);
+
         _sceneController.onReady += SceneReady;
         _physicsController.onRemoveBlue += _uiController.OnRemoveBlue;
         _physicsController.onRemoveRed += _uiController.OnRemoveRed;
         _physicsController.onOver += Over;
+
+        _State = State.Idle;
+        Spawn(_gameConfig);
     }
 
     void Init(GameConfig config)
@@ -40,24 +60,57 @@ public
 
     public void StartSim()
     {
-        _physicsController.MoveStart();
-        _uiController.HidePlayBtn();
-        _timeStart = Time.time;
+        if (_State == State.Ready)
+        {
+            _physicsController.MoveStart();
+            _uiController.HidePlayBtn();
+            _timeStart = Time.time;
+            _State = State.Playing;
+        }
+    }
+
+    void Spawn(GameConfig gameConfig)
+    {
+        if (_State == State.Idle)
+        {
+            _State = State.Loading;
+            _sceneController.Spawn(gameConfig);            
+        }
     }
 
     public void SceneReady()
     {
         _uiController.ShowPlayBtn();
+        _State = State.Ready;
     }
 
     public void Over(Side side)
     {
         float time = Time.time - _timeStart;
         _uiController.ShowGameOver(time, side);
+        _State = State.Over;
     }
 
     public void Reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void OnStateChange()
+    {
+        switch(_State)
+        {
+            case State.Loading:
+                {
+                    _uiController.HidePlayBtn();
+                    break;
+                }
+
+            case State.Ready:
+                {
+                    _uiController.ShowPlayBtn();
+                    break;
+                }
+        }
     }
 }
