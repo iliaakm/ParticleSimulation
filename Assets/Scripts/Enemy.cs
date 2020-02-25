c7412b5acb0178c99152c3b4caa993513b44a717
iliaakm <iliaakm@gmail.com>
2020-02-25 17:56:48 +0300

    add enemy properties

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,17 +9,28 @@ public enum Side
 
 public class Enemy : MonoBehaviour
 {
-    public Side _side;
-    public float _radius;
-    public float _speed;
-    public Vector2 _pos;
-    public Vector2 _areaSize;
-    public Vector2 _currentCell;
-    public Vector2 _cellSize;
+    public float Radius { get; private set; }
+    public float Speed { get; private set; }
+    public Side Side { get; private set; }
+    public Vector2 CurrentCell { get; private set; }
+    public Vector2 Pos { get; private set; }
+    public Vector2 Target { get => _target; set => _target = value; }
 
-    public Vector2 _target;
-    int _axis;
-    bool _canMove;
+    Vector2 _cellSize;
+    Vector2 _areaSize;               //размер зоны
+    int _axis;                       //текущая ось
+    bool _canMove;                   //можем двигаться
+    private Vector2 _target;
+
+    public void Init(float radius, float speed, Side side, Vector2 pos, Vector2 areaSize, Vector2 cellSize)
+    {
+        Speed = speed;
+        Side = side;
+        SetPos(pos);
+        SetRadius(radius);
+        _areaSize = areaSize;
+        _cellSize = cellSize;
+    }
 
     public void StartMovement(bool redirect = true)         //начать(продолжить) движение
     {
@@ -30,7 +41,7 @@ public class Enemy : MonoBehaviour
 
     public void ReDirection()                               //задать новое направление    
     {
-        _target = Vector2.zero;
+        Target = Vector2.zero;
         if (_axis == 0)
             _axis = RandomSign();        //-1 - X, 1 - Y
         else
@@ -38,24 +49,27 @@ public class Enemy : MonoBehaviour
 
         if (_axis < 0)
         {
-            _target.x = RandomSign() * _areaSize.x / 2;
-            _target.y = Random.Range(0, _areaSize.y) - _areaSize.y / 2;
+
+            float x = RandomSign() * _areaSize.x / 2;
+            float y = Random.Range(0, _areaSize.y) - _areaSize.y / 2;
+            Target = new Vector2(x, y);
         }
         else
         {
-            _target.y = RandomSign() * _areaSize.y / 2;
-            _target.x = Random.Range(0, _areaSize.x) - _areaSize.x / 2;
+            float y = RandomSign() * _areaSize.y / 2;
+            float x = Random.Range(0, _areaSize.x) - _areaSize.x / 2;
+            Target = new Vector2(x, y);
         }
     }
 
     private void Move()                                     //обсчет движения
     {
-        transform.localPosition = Vector2.MoveTowards(transform.localPosition, _target, _speed * Time.fixedDeltaTime);
-        _pos = transform.localPosition + (Vector3)_areaSize / 2;
-        _currentCell = new Vector2(Mathf.RoundToInt(_pos.x / _cellSize.x), Mathf.RoundToInt(_pos.y / _cellSize.y));
+        transform.localPosition = Vector2.MoveTowards(transform.localPosition, Target, Speed * Time.fixedDeltaTime);
+        Pos = transform.localPosition + (Vector3)_areaSize / 2;
+        CurrentCell = new Vector2(Mathf.RoundToInt(Pos.x / _cellSize.x), Mathf.RoundToInt(Pos.y / _cellSize.y));
 
-        _pos = transform.localPosition;
-        if (_pos == _target)
+        Pos = transform.localPosition;
+        if (Pos == Target)
             ReDirection();
     }
 
@@ -71,13 +85,13 @@ public class Enemy : MonoBehaviour
 
     public void SetRadius(float radius)                     //задать радиус
     {
-        _radius = radius;
+        Radius = radius;
         transform.localScale = Vector3.one * radius;
     }
 
     public void SetPos(Vector2 pos)                         //задать позицию
     {
         transform.localPosition = pos;
-        _pos = pos;
+        Pos = pos;
     }
 }
