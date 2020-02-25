50906032770aafc8215cdda34b79b88270764c82
iliaakm <iliaakm@gmail.com>
2020-02-25 11:44:33 +0300

    add SaveConfig

---
+++ Assets/Scripts/JSON/SaveConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SaveConfig
+{
+    public string id;
+    public Side side;
+    public float radius;
+    public float speed;
+    public Vector2 pos;
+}
+
+[Serializable]
+class SaveConfigList
+{
+    public SaveConfig[] saveConfigs;
+}
